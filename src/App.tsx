efcae23cc4ee35e6e69defa6478ecd4918afbc87
retkesjoe <szabo.gabor.com@gmail.com>
2023-09-17 18:53:48 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg'
+import { Counter } from './features/counter/Counter'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!!!</h1>
+    </div>
+  )
+}
+
+export default App
